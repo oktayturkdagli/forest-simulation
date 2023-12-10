e6ed22489852571dcbc298ba63dd2ba19107bd10
Oktay Türkdağlı <oktayturkdagli@gmail.com>
2023-12-10 21:01:06 +0300

    feat: add animation controller and animal controller

---
+++ Assets/Scripts/Core/AnimalAnimatorController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Core
+{
+    public class AnimalAnimatorController : MonoBehaviour
+    {
+        private NavMeshAgent _agent;
+        private Animator _animator;
+        private static readonly int Speed = Animator.StringToHash("Speed");
+        
+        private void Start()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+            _animator = GetComponent<Animator>();
+        }
+        
+        private void Update()
+        {
+            var speed = _agent.velocity.magnitude;
+            _animator.SetFloat(Speed, speed);
+        }
+    }
+}
---
+++ Assets/Scripts/Core/AnimalController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Core
+{
+    public class AnimalController : MonoBehaviour
+    {
+        [field: SerializeField] public float WanderRadius { get; set; } = 10f;
+        [field: SerializeField] public float WanderTimer { get; set; } = 5f;
+        
+        private Transform _target;
+        private NavMeshAgent _agent;
+        private float _timer;
+        
+        private void OnEnable()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+            _timer = WanderTimer;
+        }
+        
+        private void Update()
+        {
+            _timer += Time.deltaTime;
+            
+            if (_timer >= WanderTimer)
+            {
+                Vector3 newPos = RandomNavSphere(transform.position, WanderRadius, -1);
+                _agent.SetDestination(newPos);
+                _timer = 0;
+            }
+        }
+
+        private static Vector3 RandomNavSphere(Vector3 origin, float dist, int layerMask)
+        {
+            Vector3 randDirection = Random.insideUnitSphere * dist;
+            randDirection += origin;
+            NavMesh.SamplePosition(randDirection, out var navHit, dist, layerMask);
+            return navHit.position;
+        }
+    }
+}
